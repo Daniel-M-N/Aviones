17653d8e4af6dd3fda98930346b15c35e22e5e30
Daniel-M-N <daniel.marnav.1@educa.jcyl.es>
2025-05-07 13:22:41 +0200

    añadido por otro usuario

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+import java.io.FileWriter;
+import java.io.IOException;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    Escribiendo x = new Escribiendo();
+        x.escribir();
+}
